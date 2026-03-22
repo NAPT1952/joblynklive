c62884299f929436e1bae4981d0c2daea960bd34
NAPT1952/joblynklive <65670888+RestaurantBarManager@users.noreply.github.com>
2026-03-22 11:55:08 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import About from './pages/About';
+import Pricing from './pages/Pricing';
+import Disclaimer from './pages/Disclaimer';
+import Support from './pages/Support';
+import PayPortal from './pages/PayPortal';
+import ResumeTools from './pages/ResumeTools';
 
 function App() {
   return (
@@ -8,6 +14,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/disclaimer" element={<Disclaimer />} />
+        <Route path="/support" element={<Support />} />
+        <Route path="/payportal" element={<PayPortal />} />
+        <Route path="/resumetools" element={<ResumeTools />} />
       </Routes>
     </Router>
   );
