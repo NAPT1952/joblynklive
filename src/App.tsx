4fade7df76fdc4d1219da9d605409044ba1190df
NAPT1952/joblynklive <65670888+RestaurantBarManager@users.noreply.github.com>
2026-03-22 12:10:48 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+// Pages
 import Home from './pages/Home';
 import About from './pages/About';
 import Pricing from './pages/Pricing';
@@ -8,18 +10,33 @@ import Support from './pages/Support';
 import PayPortal from './pages/PayPortal';
 import ResumeTools from './pages/ResumeTools';
 
+// Optional (if you created this page later)
+import EmployersData from './pages/EmployersData';
+
+// Sidebar / Menu (BrainGrid)
+import BrainGrid from './components/BrainGrid';
+
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/disclaimer" element={<Disclaimer />} />
-        <Route path="/support" element={<Support />} />
-        <Route path="/payportal" element={<PayPortal />} />
-        <Route path="/resumetools" element={<ResumeTools />} />
-      </Routes>
+      {/* Sidebar Menu */}
+      <BrainGrid />
+
+      {/* Main Content */}
+      <div style={{ marginLeft: '180px' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/disclaimer" element={<Disclaimer />} />
+          <Route path="/support" element={<Support />} />
+          <Route path="/payportal" element={<PayPortal />} />
+          <Route path="/resumetools" element={<ResumeTools />} />
+
+          {/* Protected page (we’ll secure later) */}
+          <Route path="/employersdata" element={<EmployersData />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
