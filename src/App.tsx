aca9b4a2bad2ed60a318313bbd82a38448d37140
NAPT1952/joblynklive <65670888+RestaurantBarManager@users.noreply.github.com>
2026-03-25 18:19:17 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Chatbot from './components/Chatbot';
+import Home from './pages/Home';
 
 function App() {
-  return <Home />
+  return (
+    <Router>
+      <div className="min-h-screen flex flex-row">
+        {/* Main Content Area */}
+        <div className="flex-1 flex flex-col">
+          <Navbar />
+          
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              {/* You can add more routes here later, like /jobs */}
+            </Routes>
+          </main>
+
+          <Footer />
+        </div>
+
+        {/* Floating Components */}
+        <Chatbot />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
