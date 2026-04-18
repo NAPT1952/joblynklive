a04edbf9eea3fb15ee22a05d29b38d857b89e5cb
NAPT1952 <napt1952@gmail.com>
2026-04-18 21:04:15 +0530

    minimal app test

---
+++ App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{padding:'40px',textAlign:'center'}}>
+      <h1>JobLynk.live</h1>
+      <p>Site is working.</p>
+    </div>
+  )
+}
+
+export default App
